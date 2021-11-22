49a74f41feb93ee657acf20c88e4672989e2caf9
bhavik <bhavikmehta2001@gmail.com>
2021-11-23 04:05:18 +0530

    added tracking inst transfer

---
+++ INST_Tracking_agent/src/constants.js
@@ -0,0 +1,11 @@
+const INST_ADDRESS = "0x6f40d4A6237C257fff2dB00FA0510DeEECd303eb";
+const INST_DECIMALS = 18;
+const TRANSFER_EVENT =
+  "event Transfer(address indexed from, address indexed to, uint value)";
+
+
+module.exports = {
+  INST_ADDRESS,
+  INST_DECIMALS,
+  TRANSFER_EVENT,
+};
